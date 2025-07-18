545fff4e8c26faa6cdb19a7975a8752b52eb0e2e
inquerafael <playerong@gmail.com>
2025-07-18 18:27:38 -0300

    fix

---
+++ src/main/java/br/com/ifba/curso/view/CursoSave/SpringApplication.java
@@ -0,0 +1,14 @@
+
+package br.com.ifba.curso.view.CursoSave;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author inque
+ */
+public class SpringApplication {
+    public static void main(String[] args) {
+        
+    }
+ 
+}
